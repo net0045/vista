d29ed26ea04750581e38f6bd800fa8dc51f04188
Stepan Netolicky <netolst1@uhk.cz>
2025-04-10 16:12:18 +0200

    new configuration for project

--- README.md
+++
@@ -1,3 +0,0 @@
-# vista
-Hello World!
-negbagr
---
+++ vista-app/vite.config.js
@@ -0,0 +1,29 @@
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default {
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt'],
+      manifest: {
+        name: 'Moje PWA',
+        short_name: 'PWA',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+}
