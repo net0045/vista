e3cd04502a57c2c9874feda0606e20e266300ab5
Stepan Netolicky <netolst1@uhk.cz>
2025-04-10 19:51:13 +0200

    Account and Login pages

--- vista-app/src/App.jsx
+++ vista-app/src/App.jsx
@@ -1,39 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import Login from './Login';
+import Account from './Account';
 
 function App() {
   const [progress, setProgress] = useState(0);
+  const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setProgress((prev) => {
-        if (prev >= 100) {
-          clearInterval(interval);
-          return 100;
-        }
-        return prev + 1;
-      });
-    }, 20); 
+    if (location.pathname === '/') {
+      const interval = setInterval(() => {
+        setProgress((prev) => {
+          if (prev >= 100) {
+            clearInterval(interval);
+            navigate('/login');
+            return 100;
+          }
+          return prev + 1;
+        });
+      }, 20);
 
-    return () => clearInterval(interval);
-  }, []);
+      return () => clearInterval(interval); // bude se volat jen, pokud jsme byli na '/'
+    }
+    // pokud nejsme na '/', nic se nevrací a není co rušit
+    return;
+  }, [location.pathname, navigate]);
 
   return (
-    <div className="content">
-      <div className="logo">
-        <img
-          src="./src/assets/logo-vista.png" // Replace with your wallet image URL
-          alt="Logo studentské koleje Vista"
-          className="logo-image-vista"
-        />
-      </div>
-      <div className="progress-bar-container">
-        <div
-          className="progress-bar"
-          style={{ width: `${progress}%` }}
-        ></div>
-      </div>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div className="content">
+            <div className="logo">
+              <img
+                src="./src/assets/logo-vista.png"
+                alt="Logo studentské koleje Vista"
+                className="logo-image-vista"
+              />
+            </div>
+            <div className="progress-bar-container">
+              <div
+                className="progress-bar"
+                style={{ width: `${progress}%` }}
+              ></div>
+            </div>
+          </div>
+        }
+      />
+      <Route path="/login" element={<Login />} />
+      <Route path="/account" element={<Account />} />
+    </Routes>
   );
 }
 
