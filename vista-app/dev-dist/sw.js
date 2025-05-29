b1137adc150064785fdb6099f8c68bc551f0918d
Stepan Netolicky <netolst1@uhk.cz>
2025-05-29 11:54:43 +0200

    add: some github sw files

--- vista-app/dev-dist/sw.js
+++ vista-app/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2suumtifvro"
+    "revision": "0.3d9cnsa0ngg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
