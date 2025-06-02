8247141ce3382e31ac8b17046957ab2e24dd53a5
WanielDeber <weber.dan@email.cz>
2025-06-02 17:04:17 +0200

    komprese gifu

--- vista-app/src/MyOrders.jsx
+++ vista-app/src/MyOrders.jsx
@@ -66,7 +66,7 @@ function MyOrders() {
           ) : orders.length === 0 ? (
             <div className="emptySection">
               <p className='emptyText'>Asi nemáš hlad, bo tu nic nemáš</p>
-              <img src="images/hungry.gif" className='food-hungry-img' alt="" />
+              <img src="images/no-orders.gif" className='food-hungry-img' alt="" />
               <p className='emptyText'>Zkus zčekovat týdenní meníčko, třeba si vybereš nějakej gáblik</p>
               <button id='menuButton' onClick={() => navigate('/menu')}>MENU</button>
             </div>
