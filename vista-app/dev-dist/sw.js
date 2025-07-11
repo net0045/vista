a407cad3d8783648460ecf842b9d00914f55cb65
Stepan Netolicky <netolst1@uhk.cz>
2025-07-11 17:27:04 +0200

    safe commit

--- vista-app/dev-dist/sw.js
+++ vista-app/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mt5iv0ktfr8"
+    "revision": "0.3ac917rlbno"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
