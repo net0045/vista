7c8030cef1d315b5c8d8c187abde1ef0f2809792
WanielDeber <weber.dan@email.cz>
2025-07-28 22:48:26 +0200

    vzhled Payment.jsx

--- vista-app/dev-dist/sw.js
+++ vista-app/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qjv6s7rh5q8"
+    "revision": "0.neuj4k5t3c8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- vista-app/src/Payment.jsx
+++ vista-app/src/Payment.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useSearchParams, useNavigate } from 'react-router-dom';
-import './Account.css';
+import './Payment.css';
 import { getCookie, verifyToken, getSecretKey } from './lib/jwtHandler';
 
 function Payment() {
@@ -10,6 +10,10 @@ function Payment() {
   const [status, setStatus] = useState(null);
   const [orderId, setOrderId] = useState(null);
   const [message, setMessage] = useState('');
+  const [messageENG, setMessageENG] = useState('');
+  const [info, setInfo] = useState('');
+  const [infoENG, setInfoENG] = useState('');
+  const [success, setSuccess] = useState('');
 
   useEffect(() => {
     const id = params.get('id');
@@ -19,9 +23,17 @@ function Payment() {
     setStatus(stat);
 
     if (stat === 'success') {
-      setMessage('✅ Platba proběhla úspěšně. Děkujeme!');
+      setSuccess('✅');
+      setMessage('Platba proběhla úspěšně. Děkujeme!');
+      setENGMessage('The payment was successful. Thank you!');
+      setInfo('Objednávku máte v MOJE OBJEDNÁVKY');
+      setInfoENG('You can find your order in MY ORDERS');
     } else if (stat === 'fail') {
-      setMessage('❌ Platba byla zamítnuta nebo přerušena.');
+      setSuccess('❌');
+      setMessage('Platba byla zamítnuta nebo přerušena.');
+      setMessageENG('The payment was declined or interrupted.');
+      setInfo('ZKUSTE TO PROSÍM ZNOVU');
+      setInfoENG('PLEASE TRY AGAIN');
     } else {
       setMessage('⚠️ Stav platby není známý.');
     }
@@ -31,11 +43,13 @@ function Payment() {
     <div className="content">
       <div className="payment-status">
         <h2>Stav platby</h2>
+        <div className='no-yes'>{success}</div>
         <p>{message}</p>
+        <p className='eng-message'>{messageENG}</p>
+        <p className='info'>{info}</p>
+        <p className='info eng-message'>{infoENG}</p>
         {orderId && <p><strong>ID objednávky:</strong> {orderId}</p>}
-        <button className="btn" onClick={() => navigate('/account')}>
-          Zpět na účet
-        </button>
+        <button className="btn" onClick={() => navigate('/account')}>Zpět na účet / Back to account</button>
       </div>
     </div>
   );
